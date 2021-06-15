a288351ca7c4cec304d61448364bf551882e6adb
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-06-15 15:50:26 +0100

    Update App.js

--- reactcomponent_checkpoint/src/App.js
+++ reactcomponent_checkpoint/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import FullName from "./component/profile/fullname";
 import Adress from "./component/profile/address";
 import Photo from "./component/profile/profilephoto";
-import { Button } from "react-bootstrap";
+import { Button, Spinner } from "react-bootstrap";
 function App() {
   return (
     <div className="portfolio">
@@ -12,7 +12,11 @@ function App() {
         <FullName />
         <Adress />
       </div>
-      <Button variant="primary">Primary</Button>{" "}
+      <Spinner animation="border" size="sm" role="status"></Spinner>
+      <p> loading image </p>
+      <Button className="btn-style" size="sm" variant="outline-dark">
+        This is a button
+      </Button>{" "}
     </div>
   );
 }
